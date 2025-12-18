e9ac70e6b2933d20f9d4abafc434a3ae506693d9
Cybro <cybro.dev7@gmail.com>
2025-12-18 16:06:52 +0545

    Update policyGraph.js

--- content/policyGraph.js
+++ content/policyGraph.js
@@ -2,18 +2,20 @@ export class PolicyGraph {
     evaluate(intentState) {
         const policies = [];
 
-        if (intentState.scrollVelocity > 0.6) {
+        if (intentState.scrollVelocity > 0.4) {
             policies.push("slow-feed");
         }
 
-        if (intentState.hesitation > 0.4) {
+        if (intentState.hesitation > 0.3) {
             policies.push("simplify-text");
         }
 
-        if (intentState.selection > 0.3) {
+        if (intentState.selection > 0.15) {
             policies.push("highlight-context");
         }
 
         return policies;
     }
 }
+
+
